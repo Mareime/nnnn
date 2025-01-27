e28436eaf72f0e00d713a49e7c194fed502d7212
mariem034 <medleminemariem194@gmail.com>
2025-01-27 13:40:33 +0000

    first commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'hello evry one d'
+    return 'Hello word'
 if __name__ == '__main__':
     # app.logger.info("message")
     app.run(host='0.0.0.0',port=5000)
