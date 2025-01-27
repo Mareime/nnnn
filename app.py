ca93d9f45e5ccd5c75695f24e61df8ee85b3c08e
mariem034 <medleminemariem194@gmail.com>
2025-01-27 12:36:19 +0000

    add

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hghghewghhweghegh'
+    return 'hello evry one'
 if __name__ == '__main__':
     # app.logger.info("message")
     app.run(host='0.0.0.0',port=5000)
